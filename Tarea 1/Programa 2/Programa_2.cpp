d781a39e1a1a44c982c06cff6daf145cb6bf86e3
OsmarSantana369 <89064313+OsmarSantana369@users.noreply.github.com>
2022-08-18 06:53:11 -0500

    Create Programa_2.cpp

---
+++ Tarea 1/Programa 2/Programa_2.cpp
@@ -0,0 +1,13 @@
+// Tarea 1: Programa 2
+// Programa que resuelve una ecuación de primer grado
+
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+main(){
+setlocale(LC_ALL, "spanish");
+
+
+}
