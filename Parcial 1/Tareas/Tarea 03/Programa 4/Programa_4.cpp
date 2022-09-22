3b80031a4dfa5266b2acea781adc3f92b7954caf
OsmarSantana369 <89064313+OsmarSantana369@users.noreply.github.com>
2022-09-22 10:44:37 -0500

    Update Programa_4.cpp

--- Parcial 1/Tareas/Tarea 03/Programa 4/Programa_4.cpp
+++ Parcial 1/Tareas/Tarea 03/Programa 4/Programa_4.cpp
@@ -1,8 +1,8 @@
 //Programa 4
-/*Programa que lee un número entero positivo y hace la siguiente secuencia:
+/*Programa que lee un nÃºmero entero positivo y hace la siguiente secuencia:
     Si es par, lo divide entre 2.
     Si es impar, lo multiplica por 3 y le suma 1.
-  Repitiendo lo anterior hasta que el número sea 1. */
+  Repitiendo lo anterior hasta que el nÃºmero sea 1. */
 
 #include<iostream>
 
@@ -11,34 +11,38 @@ using namespace std;
 main()
 {
     setlocale(LC_ALL, "spanish");
-    int x, contador=0;                  //x es el número que el usuario insertará; contador representa el número de veces que se repite el procedimento
-    cout<<"\t\tPrograma que lee un número entero positivo y hace la siguiente secuencia:\n\t\t\tSi es par, lo divide entre 2.\n\t\t\tSi es impar, lo multiplica por 3 y le suma 1.\n\t\tRepitiendo lo anterior hasta que el número sea 1.\n\n";
-    cout<<"Inserte un número entero positivo: ";
+    int x, contador=0;                  //x es el nÃºmero que el usuario insertarÃ¡; contador representa el nÃºmero de veces que se repite el procedimento
+    cout<<"\t\tPrograma que lee un nÃºmero entero positivo y hace la siguiente secuencia:\n\t\t\tSi es par, lo divide entre 2.\n\t\t\tSi es impar, lo multiplica por 3 y le suma 1.\n\t\tRepitiendo lo anterior hasta que el nÃºmero sea 1.\n\n";
+    cout<<"Inserte un nÃºmero entero positivo: ";
     cin>>x;
     cout<<"\n";
 
-    if(x != 1)
+    if(1<=x)
     {
-        do
+        if(x != 1)
         {
-            contador++;
-            if(x % 2 == 0)
+            do
             {
-                cout<<x<<" es par. Por lo tanto, "<<x<<"/2 = ";
-                x=x/2;
-                cout<<x<<".\n\n";
-            }
-            else
-            {
-                cout<<x<<" es impar. Por lo tanto, 3("<<x<<") + 1 = ";
-                x=(3*x)+1;
-                cout<<x<<".\n\n";
-            }
-        }while(x != 1);
+                contador++;
+                if(x % 2 == 0)
+                {
+                    cout<<x<<" es par. Por lo tanto, "<<x<<"/2 = ";
+                    x=x/2;
+                    cout<<x<<".\n\n";
+                }
+                else
+                {
+                    cout<<x<<" es impar. Por lo tanto, 3("<<x<<") + 1 = ";
+                    x=(3*x)+1;
+                    cout<<x<<".\n\n";
+                }
+            }while(x != 1);
+        }
+        cout<<"\nEl nÃºmero de iteraciones fue: "<<contador;
     }
-    cout<<"\nEl número de iteraciones fue: "<<contador<<".\n\n";
-
-
+    else
+        cout<<"ERROR";
+    cout<<"\n\n";
 
     return 0;
 }
