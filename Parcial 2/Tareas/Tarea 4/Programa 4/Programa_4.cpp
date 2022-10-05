185bac5d91102c426a6bc411bada23c9ffc70bac
OsmarSantana369 <89064313+OsmarSantana369@users.noreply.github.com>
2022-10-05 10:53:16 -0500

    Update Programa_4.cpp

--- Parcial 2/Tareas/Tarea 4/Programa 4/Programa_4.cpp
+++ Parcial 2/Tareas/Tarea 4/Programa 4/Programa_4.cpp
@@ -1,5 +1,5 @@
 //Tarea 4: Programa 4
-//Escribe un programa que para algún entero n dado por el usuario calcule el valor de: e^x = 1 + (x/1!) + (x^2/2!) + (x^3/3!) + ... + (x^n/n!)
+//Escribe un programa que para algÃºn entero n dado por el usuario calcule el valor de: e^x = 1 + (x/1!) + (x^2/2!) + (x^3/3!) + ... + (x^n/n!)
 
 #include<iostream>
 #include<math.h>
@@ -10,8 +10,8 @@ main()
 {
     setlocale(LC_ALL, "spanish");
     int i, j, n, fac = 1;
-    float x, exp;
-    cout<<"\t\tPrograma que para algún entero n dado por el usuario calcula el valor de: e^x = 1 + (x/1!) + (x^2/2!) + (x^3/3!) + ... + (x^n/n!)\n\n";
+    float x, exp = 1;
+    cout<<"\t\tPrograma que para algÃºn entero n dado por el usuario calcula el valor de: e^x = 1 + (x/1!) + (x^2/2!) + (x^3/3!) + ... + (x^n/n!)\n\n";
     cout<<"Inserte el valor de x: ";
     cin>>x;
     cout<<"Inserte el valor de n: ";
